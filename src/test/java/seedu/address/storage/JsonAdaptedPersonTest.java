60212b5a701f6d0df82c4f0734e30f31fffcb159
Haleycopter <102632714+Haleycopter@users.noreply.github.com>
2026-03-06 17:53:43 +0800

    Update ADDRESS validation and tests

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -21,7 +21,7 @@ import seedu.address.model.person.Salary;
 public class JsonAdaptedPersonTest {
     private static final String INVALID_NAME = "R@chel";
     private static final String INVALID_PHONE = "+651234";
-    private static final String INVALID_ADDRESS = " ";
+    private static final String INVALID_ADDRESS = "9*1 Tah Ching Road";
     private static final String INVALID_EMAIL = "example.com";
     private static final String INVALID_TAG = "#friend";
     private static final String INVALID_SALARY = "two thousand";
