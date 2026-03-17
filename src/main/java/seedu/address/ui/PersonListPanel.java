885bba84707c4da74c5efcdcaf7e7419d1235a12
CH144 <177772377+CH144@users.noreply.github.com>
2026-03-17 22:38:21 +0000

    Change ListView to TilePane

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -2,11 +2,11 @@ package seedu.address.ui;
 
 import java.util.logging.Logger;
 
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.TilePane;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
 
@@ -18,32 +18,28 @@ public class PersonListPanel extends UiPart<Region> {
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
 
     @FXML
-    private ListView<Person> personListView;
+    private TilePane personTilePane;
 
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
+     *
+     * Code rewritten with help from ChatGPT.
      */
     public PersonListPanel(ObservableList<Person> personList) {
         super(FXML);
-        personListView.setItems(personList);
-        personListView.setCellFactory(listView -> new PersonListViewCell());
-    }
 
-    /**
-     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
-     */
-    class PersonListViewCell extends ListCell<Person> {
-        @Override
-        protected void updateItem(Person person, boolean empty) {
-            super.updateItem(person, empty);
-
-            if (empty || person == null) {
-                setGraphic(null);
-                setText(null);
-            } else {
-                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
-            }
+        int i = 1;
+        for (Person person : personList) {
+            personTilePane.getChildren().add(new PersonCard(person, i++).getRoot());
         }
-    }
 
+        personList.addListener((ListChangeListener<Person>) change -> {
+            personTilePane.getChildren().clear();
+
+            int j = 1;
+            for (Person person : personList) {
+                personTilePane.getChildren().add(new PersonCard(person, j++).getRoot());
+            }
+        });
+    }
 }
