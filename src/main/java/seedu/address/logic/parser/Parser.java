9d466447efaf4df6edc40eb5b89440792d346b76
CH144 <177772377+CH144@users.noreply.github.com>
2026-04-12 11:30:51 +0000

    Implement default method to check for stuck prefixes

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.parser;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -13,4 +16,18 @@ public interface Parser<T extends Command> {
      * @throws ParseException if {@code userInput} does not conform the expected format
      */
     T parse(String userInput) throws ParseException;
+
+    /**
+     * Parses {@code args} to check if {@code prefixes} is present, it has whitespace immediately preceeding.
+     * @throws ParseException if {@code args} does not conform the expected format
+     */
+    default void checkIfFlagsAreStuck(String args, Prefix... prefixes) throws ParseException {
+        for (Prefix pre : prefixes) {
+            Pattern p = Pattern.compile("(?<!\\s)" + pre); // pattern for no space before prefix
+            Matcher m = p.matcher(args);
+            if (m.find()) {
+                throw new ParseException("The prefix '" + pre + "' is missing a space before it.");
+            }
+        }
+    }
 }
